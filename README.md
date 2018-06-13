4299806ec2f7bebab848c4c47400fe9c1abad955
mansouri <mansouri@email.arizona.edu>
2018-06-13 12:39:38 -0700

    new style added to page

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # gifTastic
+
+### When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+
+### When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+
+### Under every gif, display its rating (PG, G, so on).
+
+### Add a new button by typing it in to the New Players Name bar. Then click the new button to get that players gifs. 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,10 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-// These players start as buttons
-    var topics = ["Manuel Neuer ", "Harry Kane ", "Cristiano Ronaldo ", "Sergio Ramos ", "Eden Hazard ", "Lionel Messi", "Robert Lewandowski", "John Obi Mikel", "Masoud Shojaei", "Aron Gunnarsson",];
+    // These players start as buttons
+    var topics = ["Manuel Neuer ", "Harry Kane ", "Cristiano Ronaldo ", "Sergio Ramos ", "Eden Hazard ", "Lionel Messi", "Robert Lewandowski", "Mesut Ozil", "Olivier Giroud", "Neymar", "Coutinho", "Antoine Griezmann", "Christian Eriksen", "Kevin De Bruyne"];
 
-//creates buttons for the array above
+
+    //creates buttons for the array above
     function renderButtons() {
         $("#buttons-view").empty();
 
@@ -13,70 +14,84 @@ $(document).ready(function() {
             a.attr("data-name", topics[i]);
             a.text(topics[i]);
             $("#buttons-view").append(a);
-            }
         }
-        renderButtons();
+    }
+    renderButtons();
 
 
-// on click of button display gifs
-$(document).on("click", '.player', function() {
-    var worldCup = $(this).html();
-    console.log(worldCup);
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + worldCup + "&api_key=7enNFpyFGkOkZL3LCR738wqoH3LMSD9r&limit=10";
-    console.log(queryURL);
+    // on click of button display those gifs
+    $(document).on("click", '.player', function () {
+        var worldCup = $(this).html();
+        console.log(worldCup);
 
-// Ajax Call
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).done(function(response) {
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + worldCup + "&api_key=7enNFpyFGkOkZL3LCR738wqoH3LMSD9r&limit=10";
+        console.log(queryURL);
 
-        var results = response.data;
 
+        // Ajax Call
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            var results = response.data;
+            var gifImage;
             $("#players-view").empty();
             for (var j = 0; j < results.length; j++) {
-                var imgDiv = $("<div>");
+                var imgDiv = $("<div>").addClass("col-md-4");
                 var imgView = results[j].images.fixed_height.url;
                 var still = results[j].images.fixed_height_still.url;
-            
-            var gifImage = $("<img>").attr("src", still).attr("data-animate", imgView).attr("data-still", still);
-                gifImage.on("click", playGif);
 
-            var rating = results[j].rating;
+                gifImage = $("<img>").attr("src", still).attr("data-animate", imgView).attr("data-still", still).attr("data-state", "still");
+                // $(document).on('click', gifImage, playGif);
+                var rating = results[j].rating;
                 console.log(rating);
 
-            var displayRating = $("<p>").text("Rating: " + rating);
-            $("players-view").prepend(displayRating);
+                var displayRating = $("<p>").text("Rating: " + rating);
+                $(imgDiv).prepend(displayRating);
+                $(imgDiv).prepend(gifImage);
+                $("#players-view").prepend(imgDiv);
             }
-        });     
-            
-function playGif() {
-    var state = $("this").attr("data-state");
-if (state === "still") {
-    $(this).attr('src', $(this).data('animate'));
-    $(this).attr('data-state', 'animate');
-} else{
-   $(this).attr('src', $(this).data('still'));
-   $(this).attr('data-state', 'still');
-  }
-}
-});
+            // $(document).on('click', gifImage, function(){
+            //     var state = $(this).attr("data-state");
+            //     console.log(state); 
+            // });
 
-$(document).on('click', '#add-player', function() {
-    if ($('#player-input').val().trim() == ''){
-      alert('Input can not be left blank');
-   }
-   else {
-    var players = $('#player-input').val().trim();
-    topics.push(players);
-    $('#player-input').val('');
-    renderButtons();
-    return false;
+        });
+    });
+
+    // makes images play and stop        
+    $(document).on('click', "img", playGif);
+    function playGif() {
+        var state = $(this).attr("data-state");
+        console.log(state);
+        if (state === "still") {
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        }
+    }
+
+
+
+    //adds new buttons that were typed in 
+    $(document).on('click', '#add-player', function () {
+        if ($('#player-input').val().trim() == '') {
+            alert('Input can not be left blank');
+        }
+        else {
+            var players = $('#player-input').val().trim();
+            topics.push(players);
+            $('#player-input').val('');
+            renderButtons();
+            return false;
 
         }
     });
 });
-              
+
 
 
